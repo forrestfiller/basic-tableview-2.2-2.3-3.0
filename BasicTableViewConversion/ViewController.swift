e5c802a48f4d0c5d55dc4b9b65c5ebd768fe6d36
Forrest Filler <forrestfiller@users.noreply.github.com>
2016-08-04 14:07:55 -0400

    initial commit for 3.0 branch post-conversion in beta, change to color

--- BasicTableViewConversion/ViewController.swift
+++ BasicTableViewConversion/ViewController.swift
@@ -13,12 +13,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var testTable: UITableView!
     var testArray = Array<Test>()
     
+    var red: UIColor!
+    
     override func loadView() {
-        let frame = UIScreen.mainScreen().bounds
+        let frame = UIScreen.main.bounds
         let view = UIView(frame: frame)
-        view.backgroundColor = .redColor()
+        view.backgroundColor = red
         
-        self.testTable = UITableView(frame: frame, style: .Plain)
+        self.testTable = UITableView(frame: frame, style: .plain)
         self.testTable.delegate = self
         self.testTable.dataSource = self
         view.addSubview(self.testTable)
@@ -32,25 +34,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 20
     }
 
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cellId = "cellId"
-        if let cell = tableView.dequeueReusableCellWithIdentifier(cellId) {
-            cell.textLabel?.text = "\(indexPath.row)"
+        if let cell = tableView.dequeueReusableCell(withIdentifier: cellId) {
+            cell.textLabel?.text = "\((indexPath as NSIndexPath).row)"
             cell.detailTextLabel?.text = "testing"
             return cell
         }
-        let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: cellId)
-        cell.textLabel?.text = "\(indexPath.row)"
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
+        cell.textLabel?.text = "\((indexPath as NSIndexPath).row)"
         cell.detailTextLabel?.text = "testing"
         return cell
     }
     
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // do my animation here and any pushes from here
     }
 
